4f775f701d2e8404bd2244de4416411910624225
Gloria Mwongeli Mutie <gmutie@safaricom.co.ke>
2022-03-06 08:47:10 +0300

    adding data ojects

---
+++ src/main/java/com/Gloria/moringaCrudjwt/datalayer/Entity/Student.java
@@ -0,0 +1,2 @@
+package com.Gloria.moringaCrudjwt.datalayer.Entity;public class Student {
+}
---
+++ src/main/java/com/Gloria/moringaCrudjwt/datalayer/Repositories/SubjectRepository.java
@@ -0,0 +1,2 @@
+package com.Gloria.moringaCrudjwt.datalayer.Repositories;public interface SubjectRepository {
+}
